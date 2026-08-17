5b5c43a65d2aa3e82ed4fa4c64ce13b3ec6279f8
Moin928 <moinqureshi928@gmail.com>
2026-08-17 21:17:59 +0530

    completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from "./app/App.jsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// renders the main app inside the root element
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
